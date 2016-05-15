3a7553448461b1ed791bddf534791a6100ef7a9e
Shane Liu <xiaochen.liu@fivestars.com>
2016-05-15 17:04:07 -0400

    forgot hpp file

---
+++ A1.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "cs488-framework/CS488Window.hpp"
+#include "cs488-framework/OpenGLImport.hpp"
+#include "cs488-framework/ShaderProgram.hpp"
+
+#include "grid.hpp"
+
+class A1 : public CS488Window {
+public:
+	A1();
+	virtual ~A1();
+
+protected:
+	virtual void init() override;
+	virtual void appLogic() override;
+	virtual void guiLogic() override;
+	virtual void draw() override;
+	virtual void cleanup() override;
+
+	virtual bool cursorEnterWindowEvent(int entered) override;
+	virtual bool mouseMoveEvent(double xPos, double yPos) override;
+	virtual bool mouseButtonInputEvent(int button, int actions, int mods) override;
+	virtual bool mouseScrollEvent(double xOffSet, double yOffSet) override;
+	virtual bool windowResizeEvent(int width, int height) override;
+	virtual bool keyInputEvent(int key, int action, int mods) override;
+
+private:
+	void initGrid();
+	void updateCubeData();
+	void uploadCubeDataToVbo();
+	void mapCubeVboDataToShaderAttributeLocations();
+	float * create_cube_vertices(float xPos, float yPos, float zPos, float r, float g, float b);
+	void updateActiveCell();
+	void uploadActiveCellDataToVbo();
+	void mapActiveCellVboDataToShaderAttributeLocations();
+
+	// Fields related to the shader and uniforms.
+	ShaderProgram m_shader;
+	GLint P_uni; // Uniform location for Projection matrix.
+	GLint V_uni; // Uniform location for View matrix.
+	GLint M_uni; // Uniform location for Model matrix.
+	GLint col_uni;   // Uniform location for cube colour.
+
+	// Fields related to grid geometry.
+	GLuint m_grid_vao; // Vertex Array Object
+	GLuint m_grid_vbo; // Vertex Buffer Object
+	GLuint m_cube_vao; // Cube Array Object
+	GLuint m_cube_vbo; // Cube Buffer Object
+	GLuint m_active_cell_vao; // Active Cell Array Object
+	GLuint m_active_cell_vbo; // Active Cell Buffer Object
+
+	// Matrices controlling the camera and projection.
+	glm::mat4 proj;
+	glm::mat4 view;
+
+	float ** colours;
+	int current_col;
+	int active_col, active_row;
+	int ** grid_height;
+	int ** grid_colour;
+	Grid grid;
+};
